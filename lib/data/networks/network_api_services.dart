3b00a3dbecf1c93a5bcd63e121c14368f681fadb
DhruvikPrj <dhruvik.prajapati@bypeopletechnologies.com>
2024-03-07 18:27:47 +0530

    added api logics

---
+++ lib/data/networks/network_api_services.dart
@@ -0,0 +1,64 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:getx_mvvm/data/app_exceptions.dart';
+import 'package:getx_mvvm/data/networks/base_api_services.dart';
+import 'package:http/http.dart' as http;
+
+class NetWorkApiServices extends BaseApiServices {
+  @override
+  dynamic responseJson;
+
+  @override
+  Future<dynamic> getApi(String url) async {
+    if (kDebugMode) {
+      print(url);
+    }
+    try {
+      final response =
+          await http.get(Uri.parse(url)).timeout(const Duration(seconds: 10));
+      responseJson = returnResponse(response);
+    } on SocketException {
+      throw InternetException('');
+    } on TimeoutException {
+      throw RequestTimeOut('');
+    }
+    return responseJson;
+  }
+
+  Future<dynamic> postApi(var data, String url) async {
+    if (kDebugMode) {
+      print(url);
+      print(data);
+    }
+    try {
+      final response = await http
+          .post(
+            Uri.parse(url),
+            body: jsonEncode(data),
+          )
+          .timeout(const Duration(seconds: 10));
+      responseJson = returnResponse(response);
+    } on SocketException {
+      throw InternetException('');
+    } on TimeoutException {
+      throw RequestTimeOut('');
+    }
+    return responseJson;
+  }
+
+  dynamic returnResponse(http.Response response) {
+    switch (response.statusCode) {
+      case 200:
+        dynamic responseJson = jsonDecode(response.body);
+        return responseJson;
+      case 400:
+        throw InvalidUrlException('');
+      default:
+        throw FetchDataException(
+            "Error while communication with server ${response.statusCode}");
+    }
+  }
+}
